50d119de5d22efa1bf59c4434e7e86167cd0ca64
GabrielTeo <gabrieltyx@gmail.com>
2021-01-08 15:41:30 +0800

    Modify home page

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Away',
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF22C95C),
-        scaffoldBackgroundColor: Color(0xFF143930),
+        primaryColor: Color(0xFF1eae50),
+        scaffoldBackgroundColor: Color(0xFF0d2620),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: LandingPage(),
