5d02e50017b537479e7f180cf8feffb83706bd54
GabrielTeo <gabrieltyx@gmail.com>
2021-01-08 16:43:44 +0800

    Add TextField and constants for values

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'landing.dart';
+import 'constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,8 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Away',
       theme: ThemeData.dark().copyWith(
-        primaryColor: Color(0xFF1eae50),
-        scaffoldBackgroundColor: Color(0xFF0d2620),
+        primaryColor: kAppBarColour,
+        scaffoldBackgroundColor: kBackgroundColour,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: LandingPage(),
